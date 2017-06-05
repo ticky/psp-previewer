4d59154d8e8a64d96b6c007024b139831601e323
Jessica Stokes <hello@jessicastokes.net>
2017-06-04 19:13:43 -0700

    clarity++

--- PSP Previewer/GeneratePreviewForURL.m
+++ PSP Previewer/GeneratePreviewForURL.m
@@ -13,11 +13,11 @@ void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
 
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
-    // To complete your generator please implement the function GeneratePreviewForURL in GeneratePreviewForURL.c
-    return noErr;
+  // To complete your generator please implement the function GeneratePreviewForURL in GeneratePreviewForURL.c
+  return noErr;
 }
 
 void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview)
 {
-    // Implement only if supported
+  // Implement only if supported
 }
--- PSP Previewer/GenerateThumbnailForURL.m
+++ PSP Previewer/GenerateThumbnailForURL.m
@@ -2,13 +2,13 @@
 #include <Foundation/NSData.h>
 #include <QuickLook/QuickLook.h>
 
+#define PBP_OFFSET_LENGTH 4
 #define PBP_MAGIC_LENGTH 4
 const UInt8 PBP_MAGIC[PBP_MAGIC_LENGTH] = {0x00, 0x50, 0x42, 0x50};
 
-uint16_t TwoLittleNSDataBytesToBigUInt16(NSData* data) {
-    // TODO: Make this 32-bit, just in case
+uint32_t FourLittleNSDataBytesToBigUInt32(NSData* data) {
     uint8_t *uint8data = (uint8_t *)[data bytes];
-    return ((uint16_t)uint8data[1] << 8) + (uint16_t)uint8data[0];
+    return ((uint32_t)uint8data[3] << 24) + ((uint32_t)uint8data[2] << 16) + ((uint32_t)uint8data[1] << 8) + (uint32_t)uint8data[0];
 }
 
 OSStatus GenerateThumbnailForURL(void *thisInterface,
@@ -26,52 +26,56 @@ OSStatus GenerateThumbnailForURL(void *thisInterface,
                                  CFDictionaryRef options,
                                  CGSize maxSize) {
 
-    NSLog(@"PSP Previewer executed on '%@'", url);
+  NSLog(@"PSP Previewer executed on '%@'", url);
     
-    // Get backup thumbnail (default PSP thumbnail image)
-    CFBundleRef mainBundle = CFBundleGetMainBundle();
-    CFURLRef fallbackThumbnailURL = CFBundleCopyResourceURL(mainBundle, CFSTR("ICON0"), CFSTR("PNG"), NULL);
+  // Get backup thumbnail (default PSP thumbnail image)
+  CFBundleRef mainBundle = CFBundleGetMainBundle();
+  CFURLRef fallbackThumbnailURL = CFBundleCopyResourceURL(mainBundle, CFSTR("ICON0"), CFSTR("PNG"), NULL);
 
-    CFDictionaryRef properties = NULL;
+  CFDictionaryRef properties = NULL;
 
-    // If we have permission, press on!
-    if (!QLThumbnailRequestIsCancelled(thumbnail)) {
-        // Load the file in question
-        NSData *imageData = [NSData dataWithContentsOfURL: (__bridge NSURL * _Nonnull)(url)];
+  // If we have permission, press on!
+  if (!QLThumbnailRequestIsCancelled(thumbnail)) {
+    // Load the file in question
+    NSData *imageData = [NSData dataWithContentsOfURL: (__bridge NSURL * _Nonnull)(url)];
         
-        // If the file starts with the PBP magic header, let's look at pulling out some details!
-        if ([
-             [imageData subdataWithRange:(NSRange){0, PBP_MAGIC_LENGTH}]
-                isEqualToData:[NSData dataWithBytes:PBP_MAGIC length:PBP_MAGIC_LENGTH]
-        ]) {
-            NSLog(@"valid PBP magic header found!");
+    // If the file starts with the PBP magic header, let's look at pulling out some details!
+    if ([
+      [imageData subdataWithRange:(NSRange){0, PBP_MAGIC_LENGTH}]
+        isEqualToData:[NSData dataWithBytes:PBP_MAGIC length:PBP_MAGIC_LENGTH]
+    ]) {
+      NSLog(@"valid PBP magic header found!");
             
-            uint16_t icon0_offset = TwoLittleNSDataBytesToBigUInt16([imageData subdataWithRange:(NSRange){0x0c, 4}]);
-            uint16_t icon0_offset_end = TwoLittleNSDataBytesToBigUInt16([imageData subdataWithRange:(NSRange){0x10, 4}]);
+      uint32_t icon0_offset = FourLittleNSDataBytesToBigUInt32(
+        [imageData subdataWithRange:(NSRange){0x0c, PBP_OFFSET_LENGTH}]
+      );
+      uint32_t icon0_offset_end = FourLittleNSDataBytesToBigUInt32(
+        [imageData subdataWithRange:(NSRange){0x10, PBP_OFFSET_LENGTH}]
+      );
             
-            uint16_t icon0_length = icon0_offset_end - icon0_offset;
+      uint32_t icon0_length = icon0_offset_end - icon0_offset;
 
-            if (icon0_length > 0) {
-                NSLog(@"ICON0 is at 0x%x, and 0x%x bytes long!", icon0_offset, icon0_length);
+      if (icon0_length > 0) {
+        NSLog(@"ICON0 is at 0x%x, and 0x%x bytes long!", icon0_offset, icon0_length);
 
-                // Read the icon data, and pass it to QuickLook!
-                NSData *icon0 = [imageData subdataWithRange:(NSRange){icon0_offset, icon0_length}];
+        // Read the icon data, and pass it to QuickLook!
+        NSData *icon0 = [imageData subdataWithRange:(NSRange){icon0_offset, icon0_length}];
 
-                QLThumbnailRequestSetImageWithData(thumbnail, (__bridge CFDataRef)icon0, properties);
-            } else {
-                NSLog(@"ICON0 is missing!");
-                // Pass QuickLook a fallback image
-                QLThumbnailRequestSetImageAtURL(thumbnail, fallbackThumbnailURL, properties);
-            }
-        }
-    } else {
-        NSLog(@"no PSP magic header found!");
-        QLThumbnailRequestSetImageAtURL(thumbnail, url, properties);
+        QLThumbnailRequestSetImageWithData(thumbnail, (__bridge CFDataRef)icon0, properties);
+      } else {
+        NSLog(@"ICON0 is missing!");
+        // Pass QuickLook a fallback image
+        QLThumbnailRequestSetImageAtURL(thumbnail, fallbackThumbnailURL, properties);
+      }
     }
+  } else {
+    NSLog(@"no PSP magic header found!");
+    QLThumbnailRequestSetImageAtURL(thumbnail, url, properties);
+  }
 
-    return noErr;
+  return noErr;
 }
 
 void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbnail) {
-    // Implement only if supported
+  // Implement only if supported
 }
