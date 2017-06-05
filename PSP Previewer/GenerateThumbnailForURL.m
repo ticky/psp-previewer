b688bf02208e22f358f2ab33ba2c4af02aed80d9
Jessica Stokes <hello@jessicastokes.net>
2017-06-04 18:56:49 -0700

    Tidy up more

--- PSP Previewer/GenerateThumbnailForURL.m
+++ PSP Previewer/GenerateThumbnailForURL.m
@@ -2,56 +2,66 @@
 #include <Foundation/NSData.h>
 #include <QuickLook/QuickLook.h>
 
-int PBP_MAGIC_LENGTH = 4;
-UInt8 PBP_MAGIC[4] = {
-    0x00,
-    0x50,
-    0x42,
-    0x50
-};
+#define PBP_MAGIC_LENGTH 4
+const UInt8 PBP_MAGIC[PBP_MAGIC_LENGTH] = {0x00, 0x50, 0x42, 0x50};
 
 uint16_t TwoLittleNSDataBytesToBigUInt16(NSData* data) {
+    // TODO: Make this 32-bit, just in case
     uint8_t *uint8data = (uint8_t *)[data bytes];
     return ((uint16_t)uint8data[1] << 8) + (uint16_t)uint8data[0];
 }
 
-OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize);
+OSStatus GenerateThumbnailForURL(void *thisInterface,
+                                 QLThumbnailRequestRef thumbnail,
+                                 CFURLRef url,
+                                 CFStringRef contentTypeUTI,
+                                 CFDictionaryRef options,
+                                 CGSize maxSize);
 void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbnail);
 
-OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize) {
+OSStatus GenerateThumbnailForURL(void *thisInterface,
+                                 QLThumbnailRequestRef thumbnail,
+                                 CFURLRef url,
+                                 CFStringRef contentTypeUTI,
+                                 CFDictionaryRef options,
+                                 CGSize maxSize) {
+
     NSLog(@"PSP Previewer executed on '%@'", url);
     
+    // Get backup thumbnail (default PSP thumbnail image)
     CFBundleRef mainBundle = CFBundleGetMainBundle();
-
     CFURLRef fallbackThumbnailURL = CFBundleCopyResourceURL(mainBundle, CFSTR("ICON0"), CFSTR("PNG"), NULL);
 
     CFDictionaryRef properties = NULL;
+
+    // If we have permission, press on!
     if (!QLThumbnailRequestIsCancelled(thumbnail)) {
+        // Load the file in question
         NSData *imageData = [NSData dataWithContentsOfURL: (__bridge NSURL * _Nonnull)(url)];
         
+        // If the file starts with the PBP magic header, let's look at pulling out some details!
         if ([
              [imageData subdataWithRange:(NSRange){0, PBP_MAGIC_LENGTH}]
                 isEqualToData:[NSData dataWithBytes:PBP_MAGIC length:PBP_MAGIC_LENGTH]
         ]) {
-            NSLog(@"valid PSP magic header found! reading offsets...");
-
-            uint16_t icon0_offset = TwoLittleNSDataBytesToBigUInt16([imageData subdataWithRange:(NSRange){0x0c, 2}]);
-            uint16_t icon0_offset_end = TwoLittleNSDataBytesToBigUInt16([imageData subdataWithRange:(NSRange){0x10, 2}]);
+            NSLog(@"valid PBP magic header found!");
+            
+            uint16_t icon0_offset = TwoLittleNSDataBytesToBigUInt16([imageData subdataWithRange:(NSRange){0x0c, 4}]);
+            uint16_t icon0_offset_end = TwoLittleNSDataBytesToBigUInt16([imageData subdataWithRange:(NSRange){0x10, 4}]);
             
             uint16_t icon0_length = icon0_offset_end - icon0_offset;
-            
-            if (icon0_length > 0) {
-                NSLog(@"ICON0 is at %hu, and %hu bytes long!", icon0_offset, icon0_length);
 
+            if (icon0_length > 0) {
+                NSLog(@"ICON0 is at 0x%x, and 0x%x bytes long!", icon0_offset, icon0_length);
+
+                // Read the icon data, and pass it to QuickLook!
                 NSData *icon0 = [imageData subdataWithRange:(NSRange){icon0_offset, icon0_length}];
 
                 QLThumbnailRequestSetImageWithData(thumbnail, (__bridge CFDataRef)icon0, properties);
-            
-                return noErr;
             } else {
                 NSLog(@"ICON0 is missing!");
+                // Pass QuickLook a fallback image
                 QLThumbnailRequestSetImageAtURL(thumbnail, fallbackThumbnailURL, properties);
-                return noErr;
             }
         }
     } else {
