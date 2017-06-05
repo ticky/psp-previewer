8de06db39210b309d479011a90497a34ae0575c3
Jessica Stokes <hello@jessicastokes.net>
2017-06-04 17:49:30 -0700

    Clean up, add fallback thumbnail
    fixes #1

--- PSP Previewer/GenerateThumbnailForURL.m
+++ PSP Previewer/GenerateThumbnailForURL.m
@@ -10,21 +10,20 @@ UInt8 PBP_MAGIC[4] = {
     0x50
 };
 
+uint16_t TwoLittleNSDataBytesToBigUInt16(NSData* data) {
+    uint8_t *uint8data = (uint8_t *)[data bytes];
+    return ((uint16_t)uint8data[1] << 8) + (uint16_t)uint8data[0];
+}
+
 OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize);
 void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbnail);
-
-/* -----------------------------------------------------------------------------
-    Generate a thumbnail for file
-
-   This function's job is to create thumbnail for designated file as fast as possible
-   ----------------------------------------------------------------------------- */
 
 OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize) {
     NSLog(@"PSP Previewer executed on '%@'", url);
     
-    //CFBundleRef mainBundle = CFBundleGetMainBundle();
+    CFBundleRef mainBundle = CFBundleGetMainBundle();
 
-    //CFURLRef fallbackThumbnailURL = CFBundleCopyResourceURL(mainBundle, CFSTR("ICON0"), CFSTR("PNG"), NULL);
+    CFURLRef fallbackThumbnailURL = CFBundleCopyResourceURL(mainBundle, CFSTR("ICON0"), CFSTR("PNG"), NULL);
 
     CFDictionaryRef properties = NULL;
     if (!QLThumbnailRequestIsCancelled(thumbnail)) {
@@ -35,12 +34,9 @@ OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thum
                 isEqualToData:[NSData dataWithBytes:PBP_MAGIC length:PBP_MAGIC_LENGTH]
         ]) {
             NSLog(@"valid PSP magic header found! reading offsets...");
-            
-            uint8_t *icon0_offset_little = (uint8_t *)[imageData subdataWithRange:(NSRange){0x0c, 2}].bytes;
-            uint8_t *icon0_offset_end_little = (uint8_t *)[imageData subdataWithRange:(NSRange){0x10, 2}].bytes;
 
-            uint16_t icon0_offset = ((uint16_t)icon0_offset_little[1] << 8) + (uint16_t)icon0_offset_little[0];
-            uint16_t icon0_offset_end = ((uint16_t)icon0_offset_end_little[1] << 8) + (uint16_t)icon0_offset_end_little[0];
+            uint16_t icon0_offset = TwoLittleNSDataBytesToBigUInt16([imageData subdataWithRange:(NSRange){0x0c, 2}]);
+            uint16_t icon0_offset_end = TwoLittleNSDataBytesToBigUInt16([imageData subdataWithRange:(NSRange){0x10, 2}]);
             
             uint16_t icon0_length = icon0_offset_end - icon0_offset;
             
@@ -54,8 +50,8 @@ OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thum
                 return noErr;
             } else {
                 NSLog(@"ICON0 is missing!");
-                //QLThumbnailRequestSetImageAtURL(thumbnail, fallbackThumbnailURL, properties);
-                //return noErr;
+                QLThumbnailRequestSetImageAtURL(thumbnail, fallbackThumbnailURL, properties);
+                return noErr;
             }
         }
     } else {
